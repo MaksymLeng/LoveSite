1cd6d6253f176284a72ddd341ebaa4dfae1a944d
Maksim Vlasiuk <maksym.lang@gmail.com>
2025-07-18 21:45:46 +0200

    convert video in mp4 and length 15s

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -6,8 +6,9 @@ export default function BackgroundVideo() {
             muted
             loop
             playsInline
+            preload="auto"
         >
-            <source src="/video/bg.mp4" type="video/mp4" />
+            <source src="/video/output.mp4" type="video/mp4" />
             Your browser does not support the video tag.
         </video>
     );
